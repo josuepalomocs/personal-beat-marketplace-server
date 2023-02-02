493a155ef8a0c1779d578dbe3f5b4927028f1669
Josue Palomo <josuepalomocs@gmail.com>
2023-02-01 18:55:09 -0600

    initial commit

---
+++ src/main/java/com/palomo/josue/personalbeatmarketplaceserver/PersonalBeatMarketplaceServerApplication.java
@@ -0,0 +1,13 @@
+package com.palomo.josue.personalbeatmarketplaceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalBeatMarketplaceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalBeatMarketplaceServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/palomo/josue/personalbeatmarketplaceserver/PersonalBeatMarketplaceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.palomo.josue.personalbeatmarketplaceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalBeatMarketplaceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
